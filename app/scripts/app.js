f2c3cfc6486ba7de276c90a2fceadf688afb38b9
Summer-Dong <D__yanan@163.com>
2016-08-02 21:05:56 +0800

    [N/A]project init

---
+++ app/scripts/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name calculatorApp
+ * @description
+ * # calculatorApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('calculatorApp', [
+    'ngAnimate',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name calculatorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the calculatorApp
+ */
+angular.module('calculatorApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
