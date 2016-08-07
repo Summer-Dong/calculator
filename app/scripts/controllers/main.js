fdd408a79d7c84623fed0377fbe7cb71d4d2e44f
Summer-Dong <D__yanan@163.com>
2016-08-07 21:56:12 +0800

    [#8]finish normal-calcu-numBut's func

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,8 +10,14 @@
 angular.module('calculatorApp')
 	.controller('MainCtrl', function() {
 		var vm = this;
+		// 滑动按钮点击事件
 		vm.slipFlag = true;
+		vm.results = "0";
 		vm.slip = function() {
 			return vm.flag = !vm.flag;
-		}
+		};
+		// 数字按钮点击事件
+		vm.numClick = function(num) {
+			vm.results += num;
+		};
 	});
