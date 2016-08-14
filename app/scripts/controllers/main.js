038ae0acf126a8e7942d76177d9ee08e7ea496c9
Summer-Dong <D__yanan@163.com>
2016-08-14 19:36:52 +0800

    [#8]num button's click-func,enhancement

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,20 +10,24 @@
 angular.module('calculatorApp')
 	.controller('MainCtrl', function() {
 		var vm = this;
-		// 滑动按钮点击事件
-		vm.slipFlag = true;
+
+		/*记录显示屏第一、二行内容，分别初始化为“空、0”*/
+		vm.inputs = "";
 		vm.results = "0";
+
+		/*记录是否有等号*/
+		vm.equalFlag = 0;
+
+		/*滑动按钮点击事件*/
+		vm.slipFlag = true;
 		vm.slip = function() {
 			return vm.slipFlag = !vm.slipFlag;
 		};
-		// 数字按钮点击事件
+
+		/*数字按钮点击事件*/
 		vm.numClick = function(num) {
+			if (vm.results == "0")
+				vm.results = "";
 			vm.results += num;
 		};
-		//功能键点击事件
-		
-		//"="按键的点击事件，当result框非0时，result内容移到typeIn框；当result框为0时，不做改变
-		vm.equalClick=function(){
-
-		}
 	});
