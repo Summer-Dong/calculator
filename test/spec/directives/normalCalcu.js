2ddae327675800822c1afb63e1e5dbb093c85b0c
Summer-Dong <D__yanan@163.com>
2016-08-30 22:49:03 +0800

    [#13]Fixed #13

--- test/spec/directives/normalCalcu.js
+++ test/spec/directives/normalCalcu.js
@@ -12,6 +12,7 @@ describe('Controller: MainCtrl', function() {
     it('should show the right attr of normalCalcu directive', function($compile) {
 		var normalCalcu = $compile('<normal-calcu></normal-calcu>')(scope);
 		scope.$digest();
+		expect(normalCalcu.restrict).toBe("EA");
 		expect(normalCalcu.replace).toBe(true);
 		expect(normalCalcu.templateUrl).toBe('../../views/calcu/normal.html');
 	});
